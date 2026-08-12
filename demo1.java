5cc0c96d223a77dbd61926c8ee17437333d37ce4
LOVISH GOYAL <112848018+goyallovish@users.noreply.github.com>
2026-08-12 18:43:17 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class demo1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+
+        // types of operators
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a+b);
+        System.out.println(a-b);
+    }
+}
---
+++ dfd.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class dfd {
+    public static void main(String[] args) {
+         int a = 20;
+        int b = 30;
+
+        // types of operators
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a+b);
+        System.out.println(a%b); // modules ans = 20   // gives reminder
+
+    }
+    }
+
